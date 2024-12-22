168e704e461c173e5e6a94391fc21678f5a93b18
NicollasMelo <nicollasmelo2001@gmail.com>
2024-12-22 19:57:53 -0300

    first commit of project

---
+++ src/main/java/com/estudo/sistema_de_listas/SistemaDeListasApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.sistema_de_listas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeListasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeListasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudo/sistema_de_listas/SistemaDeListasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.sistema_de_listas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeListasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
